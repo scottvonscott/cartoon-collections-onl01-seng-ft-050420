145a5af1a737606b3ae45ee361eccc619d96a75d
Scott  Keplinger <skeplin17@gmail.com>
2020-05-08 01:32:39 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -25,6 +25,6 @@ end
 def find_the_cheese(foods)
   cheese_types = ["cheddar", "gouda", "camembert"]
   foods.detect do |item|
-    item.include?(cheese_types[0..10])
+    item.include?(cheese_types[item])
   end
 end
